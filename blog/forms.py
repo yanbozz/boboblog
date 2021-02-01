12a1ea6c59a4adcf680f40f5acd2dd339b6107f8
yanbozz <yanbozhao1994@gmail.com>
2021-02-01 20:37:44 +0800

    美化博客提交页面

--- blog/forms.py
+++ blog/forms.py
@@ -6,7 +6,7 @@ from tag.utils import get_tag_list
 
 
 class PostCreateForm(forms.ModelForm):
-    tags = forms.MultipleChoiceField(choices=get_tag_list())
+    tags = forms.MultipleChoiceField(choices=get_tag_list(), label="标签")
 
     class Meta:
         model = Post
